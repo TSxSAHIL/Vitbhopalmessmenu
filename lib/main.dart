89f248912b059230e809f21518cfc3c01d6f39ae
TSxSAHIL <tssahil2001@gmail.com>
2023-04-21 09:37:59 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -76,6 +76,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 } else if (value == 'settings') {
                   // handle settings menu item
                 }
+                
               },
             ),
             Switch(
