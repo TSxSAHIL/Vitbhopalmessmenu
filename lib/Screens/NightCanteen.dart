177f61f44b13fa321c645da20b6238257dcc213d
TSxSAHIL <tssahil2001@gmail.com>
2023-04-23 09:32:42 +0530

    new

--- lib/Screens/NightCanteen.dart
+++ lib/Screens/NightCanteen.dart
@@ -49,7 +49,6 @@ class _NightCanteenState extends State<NightCanteen> {
     {'item': 'Ghee Dosa', 'price': 'Rs. 60'},
     {'item': 'Egg Dosa', 'price': 'Rs. 60'},
   ];
-
   final List<Map<String, dynamic>> _selectedItems = [];
   bool _sortAscending = true;
   //Logic for sort
