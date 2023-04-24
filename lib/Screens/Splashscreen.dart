86207210d8d8d7a92930995e83fdc2a8ea93ce09
TSxSAHIL <tssahil2001@gmail.com>
2023-04-24 09:31:18 +0530

    new

--- lib/Screens/Splashscreen.dart
+++ lib/Screens/Splashscreen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
-
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
