a84e4a041e62edaab85c32d82f673af109769248
TSxSAHIL <tssahil2001@gmail.com>
2023-04-27 09:22:17 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -78,6 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 }
               },
             ),
+            
             Switch(
               value: isDarkThemeEnabled,
               onChanged: (value) {
