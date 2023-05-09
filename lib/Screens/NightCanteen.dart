98810f7915a46c7c69b1bdd13c721030087fff8f
TSxSAHIL <tssahil2001@gmail.com>
2023-05-09 20:09:47 +0530

    "

--- lib/Screens/NightCanteen.dart
+++ lib/Screens/NightCanteen.dart
@@ -1,10 +1,128 @@
 import 'package:flutter/material.dart';
 
-class NightCanteen extends StatelessWidget {
-  const NightCanteen({super.key});
+class NightCanteen extends StatefulWidget {
+  NightCanteen({Key? key}) : super(key: key);
+
+  @override
+  _NightCanteenState createState() => _NightCanteenState();
+}
+
+class _NightCanteenState extends State<NightCanteen> {
+  final List<Map<String, dynamic>> _menuItems = [
+    {'item': 'Bread Omelette', 'price': 'Rs. 45'},
+    {'item': 'Plain Omelette', 'price': 'Rs. 35'},
+    {'item': 'Onion Omelette', 'price': 'Rs. 40'},
+    {'item': 'Boiled Egg', 'price': 'Rs. 10'},
+    {'item': 'Paneer Tikka', 'price': 'Rs. 125'},
+    {'item': 'Chicken Tikka', 'price': 'Rs. 130'},
+    {'item': 'Tandoori Chicken Full', 'price': 'Rs. 450'},
+    {'item': 'Tandoori Chicken Half', 'price': 'Rs. 225'},
+    {'item': 'Tandoori Chicken Quater', 'price': 'Rs. 115'},
+    {'item': 'Chicken-65', 'price': 'Rs. 100'},
+    {'item': 'Chilly Chicken', 'price': 'Rs. 130'},
+    {'item': 'Butter Chicken Masala', 'price': 'Rs. 150'},
+    {'item': 'Kadai Chicken Masala', 'price': 'Rs. 140'},
+    {'item': 'Chicken Masala', 'price': 'Rs. 140'},
+    {'item': 'Chicken Tikka Masala', 'price': 'Rs. 140'},
+    {'item': 'Schezwan Chicken Noodles', 'price': 'Rs. 130'},
+    {'item': 'Schezwan Chicken Rice', 'price': 'Rs. 120'},
+    {'item': 'Chicken Fried Rice', 'price': 'Rs. 130'},
+    {'item': 'Egg Fried Rice', 'price': 'Rs. 120'},
+    {'item': 'Egg Noodles', 'price': 'Rs. 120'},
+    {'item': 'Chicken Noodles', 'price': 'Rs. 130'},
+    {'item': 'Plain Naan', 'price': 'Rs. 25'},
+    {'item': 'Garlic Naan', 'price': 'Rs. 35'},
+    {'item': 'Butter Naan', 'price': 'Rs. 30'},
+    {'item': 'Plain Roti', 'price': 'Rs. 20'},
+    {'item': 'Butter Roti', 'price': 'Rs. 30'},
+    {'item': 'Butter Paneer Masala', 'price': 'Rs. 140'},
+    {'item': 'Kadhai Paneer', 'price': 'Rs. 130'},
+    {'item': 'Chilly Paneer', 'price': 'Rs. 130'},
+    {'item': 'Veg Noodles', 'price': 'Rs. 110'},
+    {'item': 'Schezwan Veg Noodles', 'price': 'Rs. 120'},
+    {'item': 'Veg Fried Rice', 'price': 'Rs. 110'},
+    {'item': 'Schezwan Veg Fried Rice', 'price': 'Rs. 125'},
+    {'item': 'Plain Dosa', 'price': 'Rs. 50'},
+    {'item': 'Masala Dosa', 'price': 'Rs. 60'},
+    {'item': 'Onion Dosa', 'price': 'Rs. 55'},
+    {'item': 'Ghee Dosa', 'price': 'Rs. 60'},
+    {'item': 'Egg Dosa', 'price': 'Rs. 60'},
+  ];
+
+  bool _sortAscending = true;
+
+  void sortItems() {
+  setState(() {
+    if (_sortAscending) {
+      _menuItems.sort((a, b) {
+        double? aPrice = double.tryParse(a['price'].substring(4));
+        double? bPrice = double.tryParse(b['price'].substring(4));
+        if (aPrice == null) return 1;
+        if (bPrice == null) return -1;
+        return aPrice.compareTo(bPrice);
+      });
+    } else {
+      _menuItems.sort((a, b) {
+        double? aPrice = double.tryParse(a['price'].substring(4));
+        double? bPrice = double.tryParse(b['price'].substring(4));
+        if (aPrice == null) return 1;
+        if (bPrice == null) return -1;
+        return bPrice.compareTo(aPrice);
+      });
+    }
+    _sortAscending = !_sortAscending;
+  });
+}
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: DataTable(
+        columns: [
+          DataColumn(label: Text('Item')),
+          DataColumn(
+              label: Row(
+            children: [
+              Text('Price'),
+              SortButton(
+                  sortAscending: _sortAscending,
+                  onPressed: sortItems,
+              ),
+            ],
+          )),
+        ],
+        rows: List<DataRow>.generate(
+          _menuItems.length,
+          (index) => DataRow(
+            cells: [
+              DataCell(Text(_menuItems[index]['item'])),
+              DataCell(Text(_menuItems[index]['price'])),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SortButton extends StatelessWidget {
+  const SortButton({
+    Key? key,
+    required this.sortAscending,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final bool sortAscending;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Icon(
+        sortAscending ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:messmenu/Screens/Block1&2.dart';
-
 import 'Screens/BoysMenuScreen.dart';
 import 'Screens/GirlsMenuScreen.dart';
 
@@ -12,6 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Menu App',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[900],
+      ),
       home: HomeScreen(),
     );
   }
@@ -29,136 +37,158 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool isBoysContainerHovered = false;
   bool isGirlsContainerHovered = false;
+  bool isDarkThemeEnabled = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("VIT BHOPAL MESS MENU"),
-        backgroundColor: const Color(0xff1D267D),
-        actions: [
-          PopupMenuButton(
-            itemBuilder: (BuildContext context) => [
-              const PopupMenuItem(
-                child: Text('Settings'),
-              ),
-              const PopupMenuItem(
-                child: Text('About'),
-              ),
-            ],
-          ),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Menu App',
+      theme: ThemeData(
+        brightness: isDarkThemeEnabled ? Brightness.dark : Brightness.light,
+        primarySwatch: Colors.blue,
       ),
-      body: Flex(
-        direction: Axis.vertical,
-        children: [
-          Expanded(
-            flex: 1,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Block12()),
-                );
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey[900],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("VIT BHOPAL MESS MENU"),
+          backgroundColor: const Color(0xff1D267D),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) => [
+                const PopupMenuItem(
+                  child: Text('Settings'),
+                ),
+                const PopupMenuItem(
+                  child: Text('About'),
+                ),
+              ],
+            ),
+            Switch(
+              value: isDarkThemeEnabled,
+              onChanged: (value) {
+                setState(() {
+                  isDarkThemeEnabled = value;
+                });
               },
-              child: MouseRegion(
-                onEnter: (event) {
-                  setState(() {
-                    isBoysContainerHovered = true;
-                  });
+            ),
+          ],
+        ),
+        body: Flex(
+          direction: Axis.vertical,
+          children: [
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Block12()),
+                  );
                 },
-                onExit: (event) {
-                  setState(() {
-                    isBoysContainerHovered = false;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
+                child: MouseRegion(
+                  onEnter: (event) {
+                    setState(() {
+                      isBoysContainerHovered = true;
+                    });
+                  },
+                  onExit: (event) {
+                    setState(() {
+                      isBoysContainerHovered = false;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/boys_hostel.jpg'),
+                          fit: BoxFit.cover,
                         ),
-                      ],
-                      image: const DecorationImage(
-                        image: AssetImage('assets/boys_hostel.jpg'),
-                        fit: BoxFit.cover,
+                        gradient: isBoysContainerHovered
+                            ? LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Colors.blueAccent,
+                                  Colors.lightBlueAccent,
+                                ],
+                              )
+                            : null,
                       ),
-                      gradient: isBoysContainerHovered
-                          ? const LinearGradient(
-                              begin: Alignment.centerLeft,
-                              end: Alignment.centerRight,
-                              colors: [
-                                Colors.transparent,
-                                Colors.black54,
-                              ],
-                            )
-                          : null,
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GirlsMenuScreen()),
-                );
-              },
-              child: MouseRegion(
-                onEnter: (event) {
-                  setState(() {
-                    isGirlsContainerHovered = true;
-                  });
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GirlsMenuScreen()),
+                  );
                 },
-                onExit: (event) {
-                  setState(() {
-                    isGirlsContainerHovered = false;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
+                child: MouseRegion(
+                  onEnter: (event) {
+                    setState(() {
+                      isGirlsContainerHovered = true;
+                    });
+                  },
+                  onExit: (event) {
+                    setState(() {
+                      isGirlsContainerHovered = false;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                        image: const DecorationImage(
+                          image: AssetImage('assets/girls_hostel.jpg'),
+                          fit: BoxFit.cover,
                         ),
-                      ],
-                      image: const DecorationImage(
-                        image: AssetImage('assets/girls_hostel.jpg'),
-                        fit: BoxFit.cover,
+                        gradient: isGirlsContainerHovered
+                            ? const LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Colors.pinkAccent,
+                                  Colors.purpleAccent,
+                                ],
+                              )
+                            : null,
                       ),
-                      gradient: isGirlsContainerHovered
-                          ? const LinearGradient(
-                              begin: Alignment.centerLeft,
-                              end: Alignment.centerRight,
-                              colors: [
-                                Colors.transparent,
-                                Colors.black54,
-                              ],
-                            )
-                          : null,
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
