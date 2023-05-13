65778ed6a7d915852e33fe4f5ba55c62ec860ec6
TSxSAHIL <tssahil2001@gmail.com>
2023-05-13 21:54:14 +0530

    Version 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ import 'Screens/BoysMenuScreen.dart';
 import 'Screens/GirlsMenuScreen.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Menu App',
-      
+
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.blue,
