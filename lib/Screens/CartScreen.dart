e5528ec0ae54fed487325ff18cb5ee39f1d86d02
TSxSAHIL <tssahil2001@gmail.com>
2023-05-17 22:46:45 +0530

    new update

--- lib/Screens/CartScreen.dart
+++ lib/Screens/CartScreen.dart
@@ -6,28 +6,80 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int itemCount = selectedItems.length;
+    final double totalAmount = selectedItems.fold(
+        0, (sum, item) => sum + double.parse(item['price'].substring(4)));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart'),
         backgroundColor: Color(0xff1D267D),
       ),
-      body: selectedItems.isNotEmpty
-          ? ListView.builder(
-              itemCount: selectedItems.length,
-              itemBuilder: (context, index) {
-                final item = selectedItems[index];
-                return ListTile(
-                  title: Text(item['item']),
-                  subtitle: Text(item['price']),
-                );
-              },
+      body: Column(
+        children: [
+          if (selectedItems.isNotEmpty)
+            Expanded(
+              child: ListView.builder(
+                itemCount: selectedItems.length,
+                itemBuilder: (context, index) {
+                  final item = selectedItems[index];
+                  return ListTile(
+                    title: Text(item['item']),
+                    subtitle: Text(item['price']),
+                  );
+                },
+              ),
             )
-          : Center(
-              child: Text(
-                'No items selected.',
-                style: TextStyle(fontSize: 18),
+          else
+            Expanded(
+              child: Center(
+                child: Text(
+                  'No items selected.',
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Total Items in Cart:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    itemCount.toString(),
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Total Amount:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Rs. ${totalAmount.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
