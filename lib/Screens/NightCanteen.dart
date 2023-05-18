39335a1e3b53fd4f721f61249437b892cdcfa5e9
TSxSAHIL <tssahil2001@gmail.com>
2023-05-18 13:18:08 +0530

    new update

--- lib/Screens/NightCanteen.dart
+++ lib/Screens/NightCanteen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: sort_child_properties_last, library_private_types_in_public_api
+// ignore_for_file: sort_child_properties_last, library_private_types_in_public_api, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -90,7 +90,6 @@ class _NightCanteenState extends State<NightCanteen> {
       }
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
