5ffb766abde2fbb28bb8e05cc4762012fce15a97
TSxSAHIL <tssahil2001@gmail.com>
2023-05-25 11:01:36 +0530

    1and2

--- lib/Screens/Block1&2.dart
+++ lib/Screens/Block1&2.dart
@@ -15,6 +15,7 @@ class Block12 extends StatelessWidget {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          
           title: const Text("Boys Hostel Menu"),
           backgroundColor: const Color(0xff0C134F),
           bottom: const TabBar(
