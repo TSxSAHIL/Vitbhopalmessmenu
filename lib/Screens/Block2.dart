cc0c283518ea453ad9710c2ae268f51894e50714
TSxSAHIL <tssahil2001@gmail.com>
2023-07-10 18:38:20 +0530

    .

--- lib/Screens/Block2.dart
+++ lib/Screens/Block2.dart
@@ -74,6 +74,7 @@ class _Block2State extends State<Block2> {
     final String dayName = DateFormat('EEEE').format(date);
     return menu[dayName] ?? [];
   }
+  
   @override
   Widget build(BuildContext context) {
     final List<String> currentMenu = getMenuForDate(selectedDate);
