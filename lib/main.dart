0a015e498513a02ce8f1e873ccf933fee033992b
TSxSAHIL <tssahil2001@gmail.com>
2023-08-10 18:49:04 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:messmenu/Screens/GirlsMenuScreen.dart';
 void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
